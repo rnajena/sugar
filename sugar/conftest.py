678a1e93d4e5400181314bfe57234ac00b81a513
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2024-02-01 22:19:15 +0100

    update conftest

--- sugar/conftest.py
+++ sugar/conftest.py
@@ -5,12 +5,14 @@ def pytest_addoption(parser):
     parser.addoption('--web', action='store_true',
                  default=False, help='enable web tests')
 
-def pytest_configure(config):
-    config.addinivalue_line('markers', 'webtest: mark test as a web test')
-
 def pytest_collection_modifyitems(config, items):
     if not config.getoption('--web'):
         skip_web = pytest.mark.skip(reason='webtest: need --web option to run')
         for item in items:
             if 'webtest' in item.keywords:
                 item.add_marker(skip_web)
+    # explicitely add filter warnings to markers so that they have a higher
+    # priority than command line options, e.g. -W error
+    for item in items:
+        for fwarn in config.getini('filterwarnings'):
+            item.add_marker(pytest.mark.filterwarnings(fwarn))
