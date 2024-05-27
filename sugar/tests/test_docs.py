5a8da5b4de7f0515e6e1818306e3d3c33b8da765
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2024-05-27 16:43:46 +0200

    add doctest test

---
+++ sugar/tests/test_docs.py
@@ -0,0 +1,28 @@
+# (C) 2024, Tom Eulenfeld, MIT license
+
+from contextlib import redirect_stdout
+import io
+
+
+def doctest_module(m):
+    from doctest import testmod
+    raised = False
+    try:
+        testmod(m, raise_on_error=True)
+    except Exception:
+        raised = True
+    if raised:
+        report = io.StringIO()
+        with redirect_stdout(report):
+            testmod(m, report=True)
+        assert report.getvalue() == ''
+
+
+def test_docs_data():
+    from sugar import data
+    doctest_module(data)
+
+
+def test_docs_core():
+    from sugar import core
+    doctest_module(core)
