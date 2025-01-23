f8bc02e8198f328214c09adb06fd853e84861581
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2025-01-23 16:34:41 +0100

    remove unused table cration code in __init__

--- src/sugar/__init__.py
+++ src/sugar/__init__.py
@@ -14,15 +14,8 @@ from sugar import data, web
 
 import os as __os
 from sugar._io.util import _insert_format_plugin_table as __insert_table
-from sugar._io.util import _create_format_plugin_table as __create_table
 __sphinx = __os.environ.get('SPHINX_BUILD') == '1'
 __insert_table('seqs', 'in', fancy=__sphinx)(read)
 __insert_table('seqs', 'out', fancy=__sphinx)(__write)
 __insert_table('fts', 'in', fancy=__sphinx)(read_fts)
 __insert_table('fts', 'out', fancy=__sphinx)(__write_fts)
-if __doc__ is not None:
-    _original_doc = __doc__
-    __doc__ = __doc__.format(
-        format_table_seqs=__create_table('seqs', 'io', fancy=__sphinx),
-        format_table_fts=__create_table('fts', 'io', fancy=__sphinx)
-        )
