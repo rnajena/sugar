3424a6bd42dd1035e81a55f213ff94680b80d22f
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2025-01-23 19:37:25 +0100

    remove line end character from magic, should fix windows fail

--- src/sugar/tests/test_io_binary_plugin.py
+++ src/sugar/tests/test_io_binary_plugin.py
@@ -8,7 +8,7 @@ from sugar.tests.util import tempfilename
 from sugar.core.seq import BioBasket, BioSeq
 binary_fmt = True
 filename_extensions_testbin = ['bintest']
-MAGIC = b'\xfe\x8abintestformat\n'
+MAGIC = b'\xfe\x8abintestformat'
 def is_testbin(f, **kw):
     return f.read(len(MAGIC)) == MAGIC
 def read_testbin(f):
@@ -16,16 +16,17 @@ def read_testbin(f):
 def iter_testbin(f):
     assert is_testbin(f)
     while line := f.readline():
-        if line != b'' and line.startswith(b'>'):
-            id_ = line[1:].decode('latin1').strip()
-        else:
-            data = line.decode('latin1').strip()
-            yield BioSeq(data, id=id_)
+        if line.strip() != b'':
+            if line.startswith(b'>'):
+                id_ = line[1:].decode('latin1').strip()
+            else:
+                data = line.decode('latin1').strip()
+                yield BioSeq(data, id=id_)
 def append_testbin(seq, f, magic=True):
     if magic:
         f.write(MAGIC)
-    f.write(b'>' + seq.id.encode('latin1') + b'\n')
-    f.write(str(seq).encode('latin1') + b'\n')
+    f.write(b'>' + seq.id.encode('latin1'))
+    f.write(str(seq).encode('latin1'))
 def write_testbin(seqs, f):
     f.write(MAGIC)
     for seq in seqs:
@@ -35,7 +36,7 @@ def write_testbin(seqs, f):
 ### define another fake Feature binary plugin based on gff
 binary_fmt_fts = True
 filename_extensions_fts_testbin = ['bintest']
-MAGIC2 = b'\xfe\x8aftsbintestformat\n'
+MAGIC2 = b'\xfe\x8aftsbintestformat'
 def is_fts_testbin(f, **kw):
     return f.read(len(MAGIC2)) == MAGIC2
 def read_fts_testbin(f):
