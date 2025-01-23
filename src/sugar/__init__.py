ad36bbfaaa1250ab22e4dda1912408df56df780d
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2025-01-23 16:42:27 +0100

    v0.5.0

--- src/sugar/__init__.py
+++ src/sugar/__init__.py
@@ -2,7 +2,7 @@
 Sugar -- A Python framework for bioinformatics
 """
 
-__version__ = '0.4.1-dev'
+__version__ = '0.5.0'
 
 from sugar.core.meta import Attr, Meta
 from sugar.core.fts import Feature, FeatureList, Location
