c53c93759b5930dd5e4ff31b4f1ed04987dffea2
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2025-06-04 00:24:36 +0200

    raise version number

--- src/sugar/__init__.py
+++ src/sugar/__init__.py
@@ -2,7 +2,7 @@
 Sugar -- A Python framework for bioinformatics
 """
 
-__version__ = '0.9.0'
+__version__ = '0.9.1-dev'
 
 from sugar.core.meta import Attr, Meta
 from sugar.core.fts import Feature, FeatureList, Location
